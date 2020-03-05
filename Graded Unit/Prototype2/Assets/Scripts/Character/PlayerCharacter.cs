f09a8603b718c55e532e8a0e2ce520285df4a670
xSpectrum16x <61431227+xSpectrum16x@users.noreply.github.com>
2020-03-05 14:57:37 +0000

    Added player direction checks
    Created a variable system that would determine the players direction by the if the players velocity was positive or negative. Using this once shooting is added the player will not be limited to only shooting right but instead can shoot in the direction they're looking in. I applied this to the player dash so now the player wont only be able to dash right

--- Graded Unit/Prototype2/Assets/Scripts/Character/PlayerCharacter.cs
+++ Graded Unit/Prototype2/Assets/Scripts/Character/PlayerCharacter.cs
@@ -30,6 +30,10 @@ public class PlayerCharacter : MonoBehaviour
     private float nextdashtime = 3;
     //References the component audiosource and creates a variable needed for the coin sound in respect to the AudioSource
     public AudioSource coinSound;
+  //A boolean made to determine what direction the player is looking in
+    private bool facingRight;
+    //A float made to check a change in the direction checking if the velocity is currently positive or negative. 
+    float direction;
     // Start is called before the first frame update
     void Start()
     {
@@ -37,16 +41,25 @@ public class PlayerCharacter : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         //Gets the component AudioSource 
         coinSound = GetComponent<AudioSource>();
+        facingRight = true;
     }
+
+    
 
     // Update is called once per frame
     void Update()
     {
         //Calls all methods
-        BasicMovement();
+        
         Jump();
         CheckGround();
         Dash();
+        
+    }
+    private void FixedUpdate()
+    {
+        //Calls BasicMovement in fixed update
+        BasicMovement();
     }
     //Method for movement
     void BasicMovement()
@@ -55,6 +68,21 @@ public class PlayerCharacter : MonoBehaviour
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
         //Moves at a fixed rate 
         transform.position += movement * Time.deltaTime * moveSpeed;
+        //Gives value to the direction as a float. This is used to compare which way the player is looking
+        direction = (Input.GetAxis("Horizontal"));
+        //If the players direction is positive and they're not looking to the right then the player is flipped
+        if (direction >0 && !facingRight)
+        {
+            //runs the function Flip
+            Flip();
+            //if the players direction is negative and they're not looking to the right then the player is flipped
+        } else if (direction <0 && facingRight)
+        {
+            //runs the function Flip
+            Flip();
+        }
+
+
     }
     //Method for jumping
     void Jump()
@@ -64,14 +92,14 @@ public class PlayerCharacter : MonoBehaviour
         {
             
             //if the space bar is pressed the player gets moved up by the set variable "jumpheight" 
-            gameObject.velocity = (new Vector2(0f, jumpheight));
+            rb.velocity = (new Vector2(0f, jumpheight));
         }
 
         //If the player isnt touching the ground BUT does have a double jump still remaining then the if statement below will trigger
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded == false && doublejump==true )
         {
             //Adds a double jump equal to the original jump (though this can be easily changed by changing the "jumpheight" to something else
-            gameObject.velocity = (new Vector2(0f, jumpheight));
+            rb.velocity = (new Vector2(0f, jumpheight));
             //Turns the doublejump boolean to false so they cant jump again
             doublejump = false;
         }
@@ -110,14 +138,42 @@ public class PlayerCharacter : MonoBehaviour
             //If the left shift key is pressed then the if statement will trigger
             if (Input.GetKeyDown(KeyCode.LeftShift))
             {
-                //States in the log the cooldown is starting again
-                print("Cooldown started");
-                //Gives 10 units of speed to the right
-                rb.velocity += Vector2.right * 10;
-                //resets the cooldown timer
-                nextdashtime = Time.time + cooldownTime;
+                //if the player is facing to the right the code for dashing to the right will active and the player will get velocity going right
+                if (facingRight == true)
+                {
+                    //States in the log the cooldown is starting again
+                    print("Cooldown started");
+                    //Gives 10 units of speed to the right
+                    rb.velocity += Vector2.right * 10;
+                    //resets the cooldown timer
+                    nextdashtime = Time.time + cooldownTime;
+                }
+                //However if the player is looking left instead of right then the player will receive velocity going left
+                else if(facingRight == false)
+                {
+                    //States in the log the cooldown is starting again
+                    print("Cooldown started");
+                    //Gives 10 units of speed to the left
+                    rb.velocity += Vector2.left * 10;
+                    //resets the cooldown timer
+                    nextdashtime = Time.time + cooldownTime;
+                }
+
             }
         }
+
+        
+    }
+    private void Flip()
+    {
+        //Inverts the direction the player 
+        facingRight = !facingRight;
+        //Rotates the player in the y axis to change for shooting
+        transform.Rotate(0f, 180f, 0f);
+        
+
+       
+        
     }
     //Checks for collisions
     private void OnTriggerEnter2D(Collider2D other)
