5a2c781ebf94614aef8180291eec1a85a4b507b1
xSpectrum16x <61431227+xSpectrum16x@users.noreply.github.com>
2020-03-13 19:39:48 +0000

    Created a primitive main menu
    Created 2 buttons a start and a quit button. Both are fully function and have different highlight and click alpha's.

---
+++ Graded Unit/Prototype2/Assets/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenu : MonoBehaviour
+{
+    public void StartGame()
+    {
+        //Using the Unity import "Scenemanager" by pressing the button the game loads the first level
+        SceneManager.LoadScene("Scene_1");
+    }
+
+    public void QuitGame()
+    {
+        //By pressing the button the game is closed
+        Application.Quit();
+    }
+}
+
