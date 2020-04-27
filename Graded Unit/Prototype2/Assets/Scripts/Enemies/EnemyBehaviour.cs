95d17aa3025ce6be467685ee606f0f6999296ba1
xSpectrum16x <61431227+xSpectrum16x@users.noreply.github.com>
2020-04-27 16:14:59 +0100

    Created a HealthBar for both enemies and the Player

--- Graded Unit/Prototype2/Assets/Scripts/Enemies/EnemyBehaviour.cs
+++ Graded Unit/Prototype2/Assets/Scripts/Enemies/EnemyBehaviour.cs
@@ -1,12 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 //WILL BE EXPANEDED UPON
 public class EnemyBehaviour : MonoBehaviour
 {//Enemy health
     public int health=5;
+    public Slider slider;
+
+    private void Start()
+    {
+        HealthBar();
+    }
+    private void HealthBar()
+    {
+        slider.maxValue = health;
+    }
+    
     private void OnTriggerEnter2D(Collider2D other)
     {
+
+        slider.value = health;
         //And this object happens to interact with a bullet
         if (other.CompareTag("Bullet"))
         {
@@ -16,7 +30,9 @@ public class EnemyBehaviour : MonoBehaviour
         //If their health is equal to 0 they die
         if (health<=0)
         {
+            //Destroys the enemy
             Destroy(gameObject);
+           
         }
     }
     
